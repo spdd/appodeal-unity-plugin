ab057f052786f2e92ea02f77d198c128de2c0602
spdd_mac <defaultqp@gmail.com>
2015-05-14 19:04:30 +0800

    added AdPosition.cs, IAppodealAdsBannerClient.cs

---
+++ AppodealAds/Api/AdPosition.cs
@@ -0,0 +1,13 @@
+﻿namespace AppodealAds.Api
+{
+	// The position of the ad on the screen.
+	public enum AdPosition
+	{
+		Top = 0,
+		Bottom = 1,
+		TopLeft = 2,
+		TopRight = 3,
+		BottomLeft = 4,
+		BottomRight = 5
+	}
+}
---
+++ AppodealAds/Common/IAppodealAdsBannerClient.cs
@@ -0,0 +1,21 @@
+using AppodealAds.Api;
+
+namespace AppodealAds.Common {
+	internal interface IAppodealAdsBannerClient {
+		// Create a banner view and add it into the view hierarchy.
+		void CreateBannerView(string appKey, AdSize adSize, AdPosition position);
+		
+		// Request a new ad for the banner view.
+		void LoadAd();
+		
+		// Show the banner view on the screen.
+		void ShowBannerView();
+		
+		// Hide the banner view from the screen.
+		void HideBannerView();
+		
+		// Destroys a banner view and to free up memory.
+		void DestroyBannerView();
+	}
+}
+
