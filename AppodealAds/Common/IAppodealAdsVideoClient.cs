abdb75c2476b63dc749fc4103fcd93a24e7bb2a5
spdd_mac <defaultqp@gmail.com>
2015-05-14 19:18:07 +0800

    added IAppodealAdsVideoClient

---
+++ AppodealAds/Common/IAppodealAdsVideoClient.cs
@@ -0,0 +1,17 @@
+﻿using AppodealAds.Api;
+
+namespace AppodealAds.Common {
+	internal interface IAppodealAdsVideoClient {
+		// Initialization AppodealAds and creates an VideoAd
+		void CreateVideoAd(string appKey);
+		
+		// Determines whether the video has loaded.
+		bool IsLoaded();
+		
+		// Shows the Videod.
+		void ShowVideoAd();
+		
+		// Destroys an Video to free up memory.
+		void DestroyVideoAd();
+	}
+}
