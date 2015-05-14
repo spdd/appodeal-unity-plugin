dabf21eec2d07e6fb181eb2755825722d9c9a855
spdd_mac <defaultqp@gmail.com>
2015-05-14 18:48:39 +0800

    added IAdListener interface

---
+++ AppodealAds/Common/IAdListener.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AppodealAds.Common
+{
+	// Interface for the methods to be invoked by the native plugin.
+	internal interface IAdListener
+	{
+		void FireAdLoaded();
+		void FireAdFailedToLoad(string message);
+		void FireAdOpened();
+		void FireAdClosing();
+		void FireAdClosed();
+		void FireAdLeftApplication();
+	}
+}
