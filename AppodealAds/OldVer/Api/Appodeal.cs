b5d2807aca11983827a9e4f81994b99194e4e5e9
spdd_mac <defaultqp@gmail.com>
2015-05-27 14:30:45 +0800

    implemented Appodeal

--- AppodealAds/OldVer/Api/Appodeal.cs
+++ AppodealAds/OldVer/Api/Appodeal.cs
@@ -18,70 +18,86 @@ namespace AppodealAds.Unity.Api
 		public const int ALL           = 127;
 		public const int ANY           = 127;
 		
-		public static void initialize(String appKey){
+		public static void initialize(String appKey)
+		{
 			client = AppodealAdsClientFactory.GetAppodealAdsClient();
 			client.initSDK(appKey);
 		}
 		
-		public static void initialize(String appKey, int adTypes){
+		public static void initialize(String appKey, int adTypes)
+		{
 			client = AppodealAdsClientFactory.GetAppodealAdsClient();
-			client.initSDK(appKey);
+			client.initSDK(appKey, adTypes);
 		}
 		
-		public static void setInterstitialCallbacks(IInterstitialAdListener listener){
-
+		public static void setInterstitialCallbacks(IInterstitialAdListener listener)
+		{
+			client.setInterstitialCallbacks (listener);
 		}
 		
-		public static void setVideoCallbacks(IVideoAdListener listener){
-
+		public static void setVideoCallbacks(IVideoAdListener listener)
+		{
+			client.setVideoCallbacks (listener);
 		}
 		
-		public static void setBannerCallbacks(IBannerAdListener listener){
-
+		public static void setBannerCallbacks(IBannerAdListener listener)
+		{
+			client.setBannerCallbacks (listener);
 		}
 		
-		public static void cache(int adTypes){
-
+		public static void cache(int adTypes)
+		{
+			client.cache (adTypes);
 		}
 		
-		public static Boolean isLoaded(int adTypes) {
-
+		public static Boolean isLoaded(int adTypes) 
+		{
+			client.isLoaded (adTypes);
 		}
 		
-		public static Boolean isPrecache(int adTypes) {
-
+		public static Boolean isPrecache(int adTypes) 
+		{
+			client.isPrecache (adTypes);
 		}
 		
-		public static Boolean show(int adTypes){
-
+		public static Boolean show(int adTypes)
+		{
+			client.show (adTypes);
 		}
 		
-		public static Boolean showWithPriceFloor(int adTypes){
-
+		public static Boolean showWithPriceFloor(int adTypes)
+		{
+			client.showWithPriceFloor (adTypes);
 		}
 		
-		public static void hide(int adTypes){
-
+		public static void hide(int adTypes)
+		{
+			client.hide (adTypes);
 		}
 		
-		public static void orientationChange(){
-
+		public static void orientationChange()
+		{
+			client.orientationChange ();
 		}
 		
-		public static void setAutoCache(int adTypes, Boolean autoCache) {
-
+		public static void setAutoCache(int adTypes, Boolean autoCache) 
+		{
+			client.setAutoCache (adTypes, autoCache);
 		}
 		
-		public static void setOnLoadedTriggerBoth(int adTypes, Boolean onLoadedTriggerBoth) {
-
+		public static void setOnLoadedTriggerBoth(int adTypes, Boolean onLoadedTriggerBoth) 
+		{
+			client.setOnLoadedTriggerBoth (adTypes, setOnLoadedTriggerBoth);
 		}
 		
-		public static void disableNetwork(String network) {
-
+		public static void disableNetwork(String network) 
+		{
+			client.disableNetwork (network);
 		}
 		
-		public static void disableLocationPermissionCheck() {
-
+		public static void disableLocationPermissionCheck() 
+		{
+			client.disableLocationPermissionCheck ();
 		}		
 	}
 }
